c37b36a3ac560442ca5c7ee18884dc0948e38872
dizpatek <dizpatek@gmail.com>
2026-03-03 02:41:31 +0300

    fix: added Dockerfile and instrumentation for persistent Northflank deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  experimental: {
+     instrumentationHook: true
+  },
   /* config options here */
   reactCompiler: true,
   transpilePackages: ['lightweight-charts'],

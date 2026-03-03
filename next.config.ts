301cc005f41e01408df8b31d8657707e889bae9c
dizpatek <dizpatek@gmail.com>
2026-03-03 03:51:55 +0300

    fix: disabled turbopack build and updated next to stable 15.1.8 to resolve symlink panic on northflank

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ['lightweight-charts'],
-  turbopack: {},
   async rewrites() {
     return [
       {

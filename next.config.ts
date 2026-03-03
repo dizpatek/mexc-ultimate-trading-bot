a698912f4138101bbadc31fb19797e8f4db0ae9f
dizpatek <dizpatek@gmail.com>
2026-03-03 03:10:08 +0300

    fix: removed unrecognized instrumentationHook from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-     instrumentationHook: true
-  },
   /* config options here */
   reactCompiler: true,
   transpilePackages: ['lightweight-charts'],
